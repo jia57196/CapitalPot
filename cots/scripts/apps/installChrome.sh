92797d2b19e4c98afe84f0e1d0511052b7100958
jia57196 <haipeng.jiang@gmail.com>
2016-04-13 18:05:04 +0800

    add script for Java

--- cots/scripts/apps/installChrome.sh
+++ cots/scripts/apps/installChrome.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+
+echo "1. install Chrome"
 sudo apt-get install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
+
+
+echo "2. install Sublime"
+sudo add-apt-repository ppa:webupd8team/sublime-text-3 
+sudo apt-get update 
+sudo apt-get install sublime-text-installer
 
--- cots/scripts/apps/installIonic.sh
+++ cots/scripts/apps/installIonic.sh
@@ -9,3 +9,6 @@ nvm install v4.4.2
 nvm use v4.4.2
 
 npm install -g cordova ionic
+
+sudo apt-get install mesa-utils
+
